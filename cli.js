8fa33072f96ccb8760e64f6f6f87418b8133ab9e
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-04-25 22:24:31 +0500

    updated

--- cli.js
+++ cli.js
@@ -10,10 +10,24 @@ const rl = readlines.createInterface({
   output: process.stdout,
 });
 
-rl.question("What is GitHub Handle?", (answer) => {
+rl.question("What is GitHub Handle?", async function (answer) {
   console.log(answer);
-
   //   rl.close();
 
   //   https://api.github.com/users/eebk99
+  const res = await axios
+    .get(`https://api.github.com/users/${answer}`)
+    .then((res) => {
+      // console.log(res.data);
+      const {
+        data: { name, company, location, bio },
+      } = res;
+
+      console.log("Name: ", name);
+      console.log("Company: ", company);
+      console.log("Location: ", location);
+      console.log("Bio: ", bio);
+
+      rl.close();
+    });
 });
